7e98e96a134c8cbafccd5660d5f0fcd41d7a949f
Hodgson <Geykel.Hodgson@team.neustar>
2020-06-27 21:32:18 -0600

    final bubble

--- bubble_chart/src/index.js
+++ bubble_chart/src/index.js
@@ -5,16 +5,23 @@ const d3 = require("d3");
 
 // change this to 'true' for local development
 // change this to 'false' before deploying
-export const LOCAL = true;
+export const LOCAL = false;
 
 // write viz code here
 const drawViz = (data) => {
-  let rowData = data.tables.DEFAULT;
+
+let rowData = data.tables.DEFAULT;
 
 // set the dimensions and margins of the graph
 var margin = {top: 40, right: 150, bottom: 60, left: 30},
-    width = 1000 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+    width = 700 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// remove the svg if it already exists
+  if (document.querySelector("svg")) {
+    let oldSvg = document.querySelector("svg");
+    oldSvg.parentNode.removeChild(oldSvg);
+  }
 
 // append the svg object to the body of the page
 var svg = d3.select("body")
@@ -42,8 +49,6 @@ var svg = d3.select("body")
     countries.add(row["DimensionCountry"][0])
   });
 
-console.log([...countries].sort())
-
   // ---------------------------//
   //       AXIS  AND SCALE      //
   // ---------------------------//
@@ -58,14 +63,14 @@ console.log([...countries].sort())
 
   // Add X axis label:
   svg.append("text")
-      .attr("text-anchor", "end")
-      .attr("x", width)
+      .attr("text-anchor", "middle")
+      .attr("x", width/2)
       .attr("y", height+50 )
-      .text("Gdp per Capita");
+      .text("Año");
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([minGDP, maxGDP])
+    .domain([minGDP-1, maxGDP+1])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y).ticks(5));
@@ -75,7 +80,7 @@ console.log([...countries].sort())
       .attr("text-anchor", "end")
       .attr("x", 0)
       .attr("y", -20 )
-      .text("Life expectancy")
+      .text("Crecimiento PIB Anual")
       .attr("text-anchor", "start")
 
   // Add a scale for bubble size
@@ -86,44 +91,8 @@ console.log([...countries].sort())
   // Add a scale for bubble color
   var myColor = d3.scaleOrdinal()
     .domain([...countries].sort())
-    .range(d3.schemeSet3);
+    .range(d3.schemeTableau10);
   
-  // ---------------------------//
-  //      TOOLTIP               //
-  // ---------------------------//
-
-  // -1- Create a tooltip div that is hidden by default:
-  var tooltip = d3.select("body")
-    .append("div")
-      .style("opacity", 0)
-      .attr("class", "tooltip")
-      .style("background-color", "black")
-      .style("border-radius", "5px")
-      .style("padding", "10px")
-      .style("color", "white")
-  
-  // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
-  var showTooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-    tooltip
-      .style("opacity", 1)
-      .html("Country: " + d.country)
-      .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
-  }
-  var moveTooltip = function(d) {
-    tooltip
-      .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
-  }
-  var hideTooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
 
    // ---------------------------//
   //       HIGHLIGHT GROUP      //
@@ -134,6 +103,7 @@ console.log([...countries].sort())
     // reduce opacity of all groups
     d3.selectAll(".bubbles").style("opacity", .05)
     // expect the one that is hovered
+    d = d.replace(/[, ]+/g, '')
     d3.selectAll("."+d).style("opacity", 1)
   }
 
@@ -152,15 +122,11 @@ console.log([...countries].sort())
     .data(rowData)
     .enter()
     .append("circle")
-      .attr("class", function(d) { return "bubbles " + d["DimensionCountry"][0] })
+      .attr("class", function(d) { return "bubbles " + d["DimensionCountry"][0].replace(/[, ]+/g, '') })
       .attr("cx", function (d) { return x(d["xDimensionDate"][0]); } )
       .attr("cy", function (d) { return y(d["yGDPMetric"][0]); } )
       .attr("r", function (d) { return z(d["EnergyMetric"][0]); } )
       .style("fill", function (d) { return myColor(d["DimensionCountry"][0]); } )
-    // -3- Trigger the functions for hover
-    .on("mouseover", showTooltip )
-    .on("mousemove", moveTooltip )
-    .on("mouseleave", hideTooltip )
 
   // ---------------------------//
     //       LEGEND              //
@@ -173,7 +139,7 @@ console.log([...countries].sort())
       .data(allgroups)
       .enter()
       .append("circle")
-        .attr("cx", width+20)
+        .attr("cx", width+30)
         .attr("cy", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
         .attr("r", 7)
         .style("fill", function(d){ return myColor(d)})
@@ -185,7 +151,7 @@ console.log([...countries].sort())
       .data(allgroups)
       .enter()
       .append("text")
-        .attr("x", width+20 + size*.8)
+        .attr("x", width+30 + size*.8)
         .attr("y", function(d,i){ return i * (size + 5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
         .style("fill", function(d){ return myColor(d)})
         .text(function(d){ return d})
